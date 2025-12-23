1fb3acfd0cf5aac42d6b762e151b68597ee45f69
Сидорова Дарья <sidorovadas@gmail.com>
2025-12-23 13:40:48 +0300

    Добавила

--- src/union.ts
+++ src/union.ts
@@ -2,7 +2,13 @@
 let id: string | number = 1;
 id = "2"
 
-type TPrice = number | null;
+type TPrice = number | null | string;
 let price: TPrice = null;
+let discountPrice: NonNullable<TPrice> = 55;
 price = 30;
 // price = "price"
+//literal
+
+type TTheme: "light" | "dark" = "light";
+let theme: TTheme = "light";
+theme = 'green';
